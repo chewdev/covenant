081c23035ddd5755faae1ba790236ca4f2f545ca
chewdev <itsbewald@gmail.com>
2018-10-21 22:35:34 -0700

    Added default value for value attribute

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -22,7 +22,7 @@ const TextFieldGroup = ({
         })}
         placeholder={placeholder}
         name={name}
-        value={value}
+        value={value || ""}
         onChange={onChange}
         disabled={disabled}
       />
@@ -35,7 +35,7 @@ const TextFieldGroup = ({
 TextFieldGroup.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
-  value: PropTypes.string.isRequired,
+  value: PropTypes.string,
   info: PropTypes.string,
   error: PropTypes.string,
   type: PropTypes.string.isRequired,
