508e15b7472a603285b4a0f00962c5d9d74d4c60
chewdev <itsbewald@gmail.com>
2018-11-03 23:46:43 -0700

    Add adminRoute prop for more strict access to private routes

--- client/src/components/common/PrivateRoute.js
+++ client/src/components/common/PrivateRoute.js
@@ -4,20 +4,29 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Initialize from "../Initialize";
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      auth.isAuthenticated === true && auth.user.exp > Date.now() / 1000 ? (
-        <Initialize>
-          <Component {...props} {...rest} />
-        </Initialize>
-      ) : (
-        <Redirect to="/" />
-      )
-    }
-  />
-);
+const PrivateRoute = ({ component: Component, auth, adminRoute, ...rest }) => {
+  const allowed = adminRoute
+    ? auth.user && auth.user.role === 4
+      ? true
+      : false
+    : true;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        allowed &&
+        auth.isAuthenticated === true &&
+        auth.user.exp > Date.now() / 1000 ? (
+          <Initialize>
+            <Component {...props} {...rest} />
+          </Initialize>
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    />
+  );
+};
 
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
