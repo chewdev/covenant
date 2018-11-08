5a981a58a7c15190de4e25013329fbb5b66151d3
chewdev <itsbewald@gmail.com>
2018-11-08 12:51:56 -0800

    Fix issue: Object properties expected to be an array return error when undefined

--- client/src/components/Initialize.js
+++ client/src/components/Initialize.js
@@ -7,16 +7,32 @@ import { getProjects } from "../actions/projectActions";
 
 class Initialize extends Component {
   componentDidMount() {
-    if (this.props.customers.customers.length === 0) {
+    if (
+      !this.props.customers ||
+      !this.props.customers.customers ||
+      this.props.customers.customers.length === 0
+    ) {
       this.props.getCustomers();
     }
-    if (this.props.employees.employees.length === 0) {
+    if (
+      !this.props.employees ||
+      !this.props.employees.employees ||
+      this.props.employees.employees.length === 0
+    ) {
       this.props.getEmployees();
     }
-    if (this.props.schedules.schedules.length === 0) {
+    if (
+      !this.props.schedules ||
+      !this.props.schedules.schedules ||
+      this.props.schedules.schedules.length === 0
+    ) {
       this.props.getSchedules();
     }
-    if (this.props.projects.projects.length === 0) {
+    if (
+      !this.props.projects ||
+      !this.props.projects.projects ||
+      this.props.projects.projects.length === 0
+    ) {
       this.props.getProjects("notcomplete");
     }
   }
