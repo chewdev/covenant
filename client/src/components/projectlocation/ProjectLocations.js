cc7230344d38d3c1360067e8236d797a362414b3
chewdev <itsbewald@gmail.com>
2018-11-09 21:07:07 -0800

    Add component to view all project locations in a table

---
+++ client/src/components/projectlocation/ProjectLocations.js
@@ -0,0 +1,148 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Spinner from "../common/Spinner";
+import TextFieldGroup from "../common/TextFieldGroup";
+import { getProjectLocations } from "../../actions/projectlocationActions";
+
+class ProjectLocations extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.getProjectLocations();
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    const { projectlocations, loading } = this.props.projectlocations;
+    let projLocContent;
+
+    if (projectlocations === null || loading) {
+      projLocContent = (
+        <tr>
+          <td />
+          <td>
+            <Spinner />
+          </td>
+          <td />
+          <td />
+        </tr>
+      );
+    } else {
+      let filteredProjLocs = projectlocations;
+      if (this.state.search) {
+        filteredProjLocs = filteredProjLocs.filter(
+          projectlocation =>
+            projectlocation.locationname &&
+            projectlocation.locationname
+              .toLowerCase()
+              .indexOf(this.state.search.toLowerCase()) !== -1
+        );
+      }
+
+      projLocContent = filteredProjLocs.map(projLoc => {
+        const phonenumber = !projLoc.phonenumber
+          ? null
+          : projLoc.phonenumber.length === 10
+            ? `(${projLoc.phonenumber.slice(0, 3)}) ${projLoc.phonenumber.slice(
+                3,
+                6
+              )}-${projLoc.phonenumber.slice(6)}`
+            : projLoc.phonenumber;
+        return (
+          <tr className="text-dark" key={projLoc._id}>
+            <td>
+              <Link to={`/projectlocations/${projLoc._id}`}>
+                {projLoc.address}
+              </Link>
+            </td>
+            <td>{projLoc.locationname || "Unavailable"}</td>
+            <td>{projLoc.contactname || "Unavailable"}</td>
+            <td>
+              <a
+                className="btn btn-link btn-lg p-0"
+                href={`tel:${phonenumber}`}
+              >
+                {phonenumber}
+              </a>
+            </td>
+          </tr>
+        );
+      });
+    }
+
+    return (
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-12">
+            <TextFieldGroup
+              placeholder="Search"
+              name="search"
+              value={this.state.search}
+              onChange={this.onChange}
+              error={null}
+              info="Search By Location Name"
+            />
+          </div>
+          <div className="col">
+            <div className="table-responsive">
+              <table className="table table-striped border border-dark">
+                <thead>
+                  <tr>
+                    <th>Project Locations</th>
+                    <th />
+                    <th />
+                    <th>
+                      {" "}
+                      <Link
+                        className="btn btn-primary btn-lg btn-block"
+                        to={"/projectlocations/new"}
+                      >
+                        Add Project Location
+                      </Link>{" "}
+                    </th>
+                  </tr>
+                </thead>
+                <thead className="thead-dark">
+                  <tr>
+                    <th>Location Address</th>
+                    <th>Location Name</th>
+                    <th>Contact</th>
+                    <th>Phone Number</th>
+                  </tr>
+                </thead>
+                <tbody>{projLocContent}</tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ProjectLocations.propTypes = {
+  getProjectLocations: PropTypes.func.isRequired,
+  projectlocations: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  projectlocations: state.projectlocation
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjectLocations }
+)(ProjectLocations);
