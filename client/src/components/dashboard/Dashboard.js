c5753210107cfb1b44f8d413164940cb783c7628
chewdev <itsbewald@gmail.com>
2018-11-10 00:03:16 -0800

    Update dashboard layout

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -10,103 +10,105 @@ export default class Dashboard extends Component {
             <div className="card text-center border-dark">
               <div className="card-header bg-dark text-white">Dashboard</div>
               <div className="card-body p-0 pt-2">
-                <div className="card-title text-primary">
-                  <h2>
-                    <strong>Add or View All</strong>
-                  </h2>
+                <div className="card-title text-secondary">
+                  <h2>What would you like to do?</h2>
                 </div>
                 <div className="list-group">
                   <div className="list-group-item">
-                    <div className="row mb-4">
-                      <div className="col-6">
+                    <div className="row mt-4">
+                      <div className="col-12 col-sm-6 col-lg-4 m-auto">
                         <h3 className="card-text">
                           <u>Schedule</u>
                         </h3>
+
                         <Link
                           to="/schedule/new"
-                          className="btn btn-primary btn-block btn-lg"
+                          className="btn btn-outline-primary btn-lg mb-4 mr-2"
                         >
                           New
                         </Link>
+
                         <Link
                           to="/schedule"
-                          className="btn btn-secondary btn-block btn-lg"
+                          className="btn btn-outline-secondary btn-lg mb-4 ml-2"
                         >
                           View All
                         </Link>
                       </div>
-                      <div className="col-6">
+                      <div className="col-12 col-sm-6 col-lg-4 m-auto">
                         <h3 className="card-text">
                           <u>Projects</u>
                         </h3>
+
                         <Link
                           to="/projects/new"
-                          className="btn btn-primary btn-block btn-lg"
+                          className="btn btn-outline-primary btn-lg mb-4 mr-2"
                         >
                           New
                         </Link>
+
                         <Link
                           to="/projects"
-                          className="btn btn-secondary btn-block btn-lg"
+                          className="btn btn-outline-secondary btn-lg mb-4 ml-2"
                         >
                           View All
                         </Link>
                       </div>
-                    </div>
-                    <div className="row mb-4">
-                      <div className="col-6">
+
+                      <div className="col-12 col-sm-6 col-lg-4 m-auto">
                         <h3 className="card-text">
                           <u>Customers</u>
                         </h3>
                         <Link
                           to="/customers/new"
-                          className="btn btn-primary btn-block btn-lg"
+                          className="btn btn-outline-primary btn-lg mb-4 mr-2"
                         >
                           New
                         </Link>
                         <Link
                           to="/customers"
-                          className="btn btn-secondary btn-block btn-lg"
+                          className="btn btn-outline-secondary btn-lg mb-4 ml-2"
                         >
                           View All
                         </Link>
                       </div>
-                      <div className="col-6">
+                      <div className="d-none d-lg-block col-lg-2" />
+                      <div className="col-12 col-sm-6 col-lg-4 m-auto">
                         <h3 className="card-text">
                           <u>Employees</u>
                         </h3>
                         <Link
                           to="/employees/new"
-                          className="btn btn-primary btn-block btn-lg"
+                          className="btn btn-outline-primary btn-lg mb-4 mr-2"
                         >
                           New
                         </Link>
                         <Link
                           to="/employees"
-                          className="btn btn-secondary btn-block btn-lg"
+                          className="btn btn-outline-secondary btn-lg mb-4 ml-2"
                         >
                           View All
                         </Link>
                       </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6 m-auto">
+
+                      <div className="col-12 col-sm-6 col-lg-4 m-auto">
                         <h3 className="card-text">
                           <u>Project Locations</u>
                         </h3>
                         <Link
                           to="/projectlocations/new"
-                          className="btn btn-primary btn-block btn-lg"
+                          className="btn btn-outline-primary btn-lg mb-4 mr-2"
                         >
                           New
                         </Link>
                         <Link
                           to="/projectlocations"
-                          className="btn btn-secondary btn-block btn-lg"
+                          className="btn btn-outline-secondary btn-lg mb-4 ml-2"
                         >
                           View All
                         </Link>
                       </div>
+                      <div className="d-none d-lg-block col-lg-2" />
                     </div>
                   </div>
                 </div>
