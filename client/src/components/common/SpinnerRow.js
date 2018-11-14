09a5f842c67f5245f587110d6202fd65b5aeab82
chewdev <itsbewald@gmail.com>
2018-11-13 23:21:23 -0800

    Add reusable Spinner table row component

---
+++ client/src/components/common/SpinnerRow.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Spinner from "./Spinner";
+
+export default function SpinnerRow() {
+  return (
+    <tr>
+      <td />
+      <td>
+        <Spinner />
+      </td>
+      <td />
+      <td />
+    </tr>
+  );
+}
