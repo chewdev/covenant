b275bcce4d2e616197b888ab48d4f84340a64d91
chewdev <itsbewald@gmail.com>
2018-11-15 22:22:37 -0800

    Add EmployeeTableRow component

---
+++ client/src/components/employee/EmployeeTableRow.js
@@ -0,0 +1,28 @@
+import React from "react";
+import TableRow from "../common/TableRow";
+import { Link } from "react-router-dom";
+import { formatPhoneNumber } from "../../utils/phoneNumbers";
+
+export default function EmployeeTableRow({ employee }) {
+  return (
+    <TableRow
+      classes="text-dark"
+      tdArray={[
+        <Link
+          to={`/employees/${employee._id}`}
+          className="btn btn-link btn-lg btn-link-mod pl-0"
+        >
+          {employee.name}
+        </Link>,
+        formatPhoneNumber(employee.phonenumber) || "Unavailable",
+        employee.title || "Unavailable",
+        <Link
+          className="btn btn-secondary"
+          to={`/employees/${employee._id}/edit`}
+        >
+          Update
+        </Link>
+      ]}
+    />
+  );
+}
