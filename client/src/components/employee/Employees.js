74831a2249d90e40480b63c7eb91b2edae537b33
chewdev <itsbewald@gmail.com>
2018-11-15 22:03:18 -0800

    Update Employees component. Large refactor into reusable components.

--- client/src/components/employee/Employees.js
+++ client/src/components/employee/Employees.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import SpinnerRow from "../common/SpinnerRow";
 import { getEmployees } from "../../actions/employeeActions";
+import employeeSelector from "../../selectors/employees";
+import ContainerRow from "../common/ContainerRow";
+import TableHead from "../common/TableHead";
+import AddLink from "../common/LgBlockPrimaryBtnLink";
+import CovTable from "../common/CovTable";
+import EmployeeTableRow from "./EmployeeTableRow";
 
 class Employees extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       search: "",
-      searchby: "name"
+      searchBy: "name"
     };
-
-    this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount() {
@@ -32,150 +34,80 @@ class Employees extends Component {
     if (employees === null || employeesloading) {
       employeeContent = <SpinnerRow />;
     } else {
-      let filteredEmployees = employees;
-      if (this.state.search) {
-        if (employees[0] && employees[0][this.state.searchby]) {
-          filteredEmployees = employees.filter(
-            employee =>
-              employee[this.state.searchby]
-                .toLowerCase()
-                .indexOf(this.state.search.toLowerCase()) !== -1
-          );
-        }
-      }
-      employeeContent = filteredEmployees.map(employee => {
-        const phonenumber = !employee.phonenumber
-          ? null
-          : employee.phonenumber.length === 10
-            ? `(${employee.phonenumber.slice(
-                0,
-                3
-              )}) ${employee.phonenumber.slice(
-                3,
-                6
-              )}-${employee.phonenumber.slice(6)}`
-            : employee.phonenumber;
-        return (
-          <tr className="text-dark" key={employee._id}>
-            <td>
-              <Link
-                to={`/employees/${employee._id}`}
-                className="btn btn-link btn-lg btn-link-mod pl-0"
-              >
-                {employee.name}
-              </Link>
-            </td>
-            <td>
-              {phonenumber ? (
-                <a
-                  href={`tel:${phonenumber}`}
-                  className="btn btn-link btn-lg btn-link-mod pl-0"
-                >
-                  {phonenumber}
-                </a>
-              ) : (
-                null || "Unavailable"
-              )}
-            </td>
-            <td>{employee.title || "Unavailable"}</td>
-            <td>
-              <Link
-                className="btn btn-secondary"
-                to={`/employees/${employee._id}/edit`}
-              >
-                Update
-              </Link>
-            </td>
-          </tr>
-        );
-      });
+      employeeContent = employeeSelector(employees || [], {
+        ...this.state
+      }).map(employee => (
+        <EmployeeTableRow employee={employee} key={employee._id} />
+      ));
     }
 
     return (
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col-12">
-            <div className="form-group">
-              <div className="input-group">
-                <div className="input-group-prepend">
-                  <button
-                    className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
+      <ContainerRow>
+        <div className="col-12">
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  {
                     {
-                      {
-                        name: "Name",
-                        title: "Title"
-                      }[this.state.searchby]
-                    }
+                      name: "Name",
+                      title: "Title"
+                    }[this.state.searchBy]
+                  }
+                </button>
+                <div className="dropdown-menu">
+                  <button
+                    className="dropdown-item"
+                    onClick={() => {
+                      this.setState({ searchBy: "name" });
+                    }}
+                  >
+                    Name
                   </button>
-                  <div className="dropdown-menu">
-                    <button
-                      className="dropdown-item"
-                      onClick={() => {
-                        this.setState({ searchby: "name" });
-                      }}
-                    >
-                      Name
-                    </button>
-                    <button
-                      className="dropdown-item"
-                      onClick={() => {
-                        this.setState({ searchby: "title" });
-                      }}
-                    >
-                      Title
-                    </button>
-                  </div>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => {
+                      this.setState({ searchBy: "title" });
+                    }}
+                  >
+                    Title
+                  </button>
                 </div>
-                <input
-                  type="text"
-                  className="form-control form-control-lg"
-                  placeholder="Search"
-                  name="search"
-                  value={this.state.search || ""}
-                  onChange={this.onChange}
-                />
               </div>
-              <small className="form-text text-muted">Search By</small>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                placeholder="Search"
+                name="search"
+                value={this.state.search || ""}
+                onChange={this.onChange.bind(this)}
+              />
             </div>
-          </div>
-          <div className="col">
-            <div className="table-responsive">
-              <table className="table table-striped border border-dark">
-                <thead>
-                  <tr>
-                    <th>Employees</th>
-                    <th />
-                    <th />
-                    <th>
-                      {" "}
-                      <Link
-                        className="btn btn-primary btn-lg btn-block"
-                        to={"/employees/new"}
-                      >
-                        Add Employee
-                      </Link>{" "}
-                    </th>
-                  </tr>
-                </thead>
-                <thead className="thead-dark">
-                  <tr>
-                    <th>Name</th>
-                    <th>Phone Number</th>
-                    <th>Title</th>
-                    <th>Update</th>
-                  </tr>
-                </thead>
-                <tbody>{employeeContent}</tbody>
-              </table>
-            </div>
+            <small className="form-text text-muted">Search By</small>
           </div>
         </div>
-      </div>
+        <CovTable>
+          <TableHead
+            thArray={[
+              "Employees",
+              null,
+              null,
+              <AddLink text="Add Employee" to={"/employees/new"} />
+            ]}
+          />
+          <TableHead
+            classes="thead-dark"
+            thArray={["Name", "Phone Number", "Title", "Update"]}
+          />
+          <tbody>{employeeContent}</tbody>
+        </CovTable>
+      </ContainerRow>
     );
   }
 }
