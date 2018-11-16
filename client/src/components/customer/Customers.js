ce6b78f8d831b0a066a04ef5f33b627286880c19
chewdev <itsbewald@gmail.com>
2018-11-15 22:04:11 -0800

    Remove unused Link import

--- client/src/components/customer/Customers.js
+++ client/src/components/customer/Customers.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import SpinnerRow from "../common/SpinnerRow";
 import TextFieldGroup from "../common/TextFieldGroup";
