85b37685218644c887454aec7d1beb88ed02122f
chewdev <itsbewald@gmail.com>
2018-11-16 18:36:44 -0800

    Display 'Schedule item not found' alert when attempting to edit a schedule item id that does not exist

--- client/src/components/schedule/AddUpdateSchedule.js
+++ client/src/components/schedule/AddUpdateSchedule.js
@@ -71,7 +71,8 @@ class AddSchedule extends Component {
     if (
       this.props.editOrAdd === "edit" &&
       !props.schedules.scheduleloading &&
-      !this.state.hasReceivedData
+      !this.state.hasReceivedData &&
+      props.schedules.schedule !== null
     ) {
       let employees = [];
       let project = "";
@@ -157,130 +158,146 @@ class AddSchedule extends Component {
 
   render() {
     const { errors } = this.state;
-    const projectOptions = this.props.projects
-      ? this.props.projects.projects
-          .filter(project => project.currentstatus !== "Completed")
-          .map(project => ({
-            label: project.projectname,
-            value: project._id
+
+    let formContent;
+
+    if (
+      this.props.editOrAdd !== "add" &&
+      this.props.schedules.schedule === null
+    ) {
+      formContent = (
+        <div className="alert alert-danger">Schedule item not found</div>
+      );
+    } else {
+      const projectOptions = this.props.projects
+        ? this.props.projects.projects
+            .filter(project => project.currentstatus !== "Completed")
+            .map(project => ({
+              label: project.projectname,
+              value: project._id
+            }))
+        : [];
+      projectOptions.unshift({
+        label: "* Select Project To Schedule",
+        value: 0
+      });
+      const employeeOptions = this.props.employees
+        ? this.props.employees.employees.map(employee => ({
+            label: employee.name,
+            value: employee._id
           }))
-      : [];
-    projectOptions.unshift({ label: "* Select Project To Schedule", value: 0 });
-    const employeeOptions = this.props.employees
-      ? this.props.employees.employees.map(employee => ({
-          label: employee.name,
-          value: employee._id
-        }))
-      : [];
-    employeeOptions.unshift({
-      label: "Select Employee to Add or Remove",
-      value: 0
-    });
+        : [];
+      employeeOptions.unshift({
+        label: "Select Employee to Add or Remove",
+        value: 0
+      });
 
-    const selectedEmployees = this.state.employees.map(emplID => {
-      const foundEmployee = this.props.employees.employees.find(
-        employee => employee._id === emplID
-      );
-      return foundEmployee ? foundEmployee.name : null;
-    });
+      const selectedEmployees = this.state.employees.map(emplID => {
+        const foundEmployee = this.props.employees.employees.find(
+          employee => employee._id === emplID
+        );
+        return foundEmployee ? foundEmployee.name : null;
+      });
 
-    const formContent =
-      (["edit", "addToProject"].includes(this.props.editOrAdd) &&
-        this.state.isLoading) ||
-      this.props.projects.projectsloading ||
-      this.props.employees.employeesloading ? (
-        <Spinner />
-      ) : this.props.editOrAdd === "edit" &&
-      this.props.schedules.schedule === null ? (
-        <div className="alert alert-danger">Schedule not found</div>
-      ) : (
-        <div>
-          <h1 className="display-4 text-center">
-            {this.props.editOrAdd === "edit" ? "Edit " : "Add "} Schedule
-          </h1>
-          <p className="lead text-center">Add schedule information below</p>
-          <small className="d-block pb-3">* = required fields</small>
-          <form onSubmit={this.onSubmit}>
-            <SelectListGroup
-              name="project"
-              value={this.state.project}
-              onChange={this.onChange}
-              error={errors.project}
-              options={projectOptions}
-              info="Select a project to schedule"
-            />
-            <div>{selectedEmployees.join(", ")}</div>
-            <SelectListGroup
-              name="employees"
-              value={""}
-              onChange={this.onAddOrRemoveEmployee}
-              error={errors.employees}
-              options={employeeOptions}
-              info="Select employees to add to schedule"
-            />
-
-            <div className="form-group">
-              <input
-                type="datetime-local"
-                className={classnames("form-control form-control-lg", {
-                  "is-invalid": errors.date || this.state.dateError
-                })}
-                name="date"
-                value={this.state.date}
+      formContent =
+        (["edit", "addToProject"].includes(this.props.editOrAdd) &&
+          this.state.isLoading) ||
+        this.props.projects.projectsloading ||
+        this.props.employees.employeesloading ? (
+          <Spinner />
+        ) : this.props.editOrAdd === "edit" &&
+        this.props.schedules.schedule === null ? (
+          <div className="alert alert-danger">Schedule not found</div>
+        ) : (
+          <div>
+            <h1 className="display-4 text-center">
+              {this.props.editOrAdd === "edit" ? "Edit " : "Add "} Schedule
+            </h1>
+            <p className="lead text-center">Add schedule information below</p>
+            <small className="d-block pb-3">* = required fields</small>
+            <form onSubmit={this.onSubmit}>
+              <SelectListGroup
+                name="project"
+                value={this.state.project}
                 onChange={this.onChange}
-                id="date"
-                min="2017-01-01T00:00"
-                max="2020-12-31T23:59"
-                style={{
-                  boxShadow: "none",
-                  WebkitAppearance: "searchfield"
-                }}
+                error={errors.project}
+                options={projectOptions}
+                info="Select a project to schedule"
               />
-              <small className="form-text text-muted">
-                Date and time formatted as (MM/DD/YYYY hh:mm:ss AM) - i.e.
-                11/22/18 02:00:00 PM
-              </small>
-              {(errors.date && (
-                <div className="invalid-feedback">{errors.date}</div>
-              )) ||
-                (this.state.dateError && (
-                  <div className="invalid-feedback">Input date is invalid</div>
-                ))}
-            </div>
-            <TextAreaFieldGroup
-              placeholder="Add Notes"
-              name="notes"
-              value={this.state.notes}
-              onChange={this.onChange}
-              error={errors.notes}
-              info="Add notes about this appointment"
-            />
-            <SelectListGroup
-              name="isComplete"
-              value={this.state.isComplete}
-              onChange={this.onChange}
-              error={errors.isComplete}
-              options={[
-                {
-                  label: "Not Complete",
-                  value: "false"
-                },
-                {
-                  label: "Complete",
-                  value: "true"
-                }
-              ]}
-              info="Select employees to add to schedule"
-            />
-            <input
-              type="submit"
-              value="Submit"
-              className="btn btn-primary btn-block mt-4"
-            />
-          </form>
-        </div>
-      );
+              <div>{selectedEmployees.join(", ")}</div>
+              <SelectListGroup
+                name="employees"
+                value={""}
+                onChange={this.onAddOrRemoveEmployee}
+                error={errors.employees}
+                options={employeeOptions}
+                info="Select employees to add to schedule"
+              />
 
+              <div className="form-group">
+                <input
+                  type="datetime-local"
+                  className={classnames("form-control form-control-lg", {
+                    "is-invalid": errors.date || this.state.dateError
+                  })}
+                  name="date"
+                  value={this.state.date}
+                  onChange={this.onChange}
+                  id="date"
+                  min="2017-01-01T00:00"
+                  max="2020-12-31T23:59"
+                  style={{
+                    boxShadow: "none",
+                    WebkitAppearance: "searchfield"
+                  }}
+                />
+                <small className="form-text text-muted">
+                  Date and time formatted as (MM/DD/YYYY hh:mm:ss AM) - i.e.
+                  11/22/18 02:00:00 PM
+                </small>
+                {(errors.date && (
+                  <div className="invalid-feedback">{errors.date}</div>
+                )) ||
+                  (this.state.dateError && (
+                    <div className="invalid-feedback">
+                      Input date is invalid
+                    </div>
+                  ))}
+              </div>
+              <TextAreaFieldGroup
+                placeholder="Add Notes"
+                name="notes"
+                value={this.state.notes}
+                onChange={this.onChange}
+                error={errors.notes}
+                info="Add notes about this appointment"
+              />
+              <SelectListGroup
+                name="isComplete"
+                value={this.state.isComplete}
+                onChange={this.onChange}
+                error={errors.isComplete}
+                options={[
+                  {
+                    label: "Not Complete",
+                    value: "false"
+                  },
+                  {
+                    label: "Complete",
+                    value: "true"
+                  }
+                ]}
+                info="Select employees to add to schedule"
+              />
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-primary btn-block mt-4"
+              />
+            </form>
+          </div>
+        );
+    }
     return (
       <div className="container">
         <div className="row my-4">
