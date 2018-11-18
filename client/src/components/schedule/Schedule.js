6766cdfb144a6298b7d50de573b5180248c25421
chewdev <itsbewald@gmail.com>
2018-11-17 23:58:11 -0800

    Refactor to use reusable components. Update layout.

--- client/src/components/schedule/Schedule.js
+++ client/src/components/schedule/Schedule.js
@@ -4,10 +4,16 @@ import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../common/Spinner";
 import ConfirmRemoveModal from "../common/ConfirmRemoveModal";
+import CardFooter from "../common/CardFooter";
+import CardHeader from "../common/CardHeader";
+import CardHeaderLink from "../common/CardHeaderLink";
+import ListGroupItemh3p from "../common/ListGroupItemh3p";
 import {
   getSchedule,
   deleteSchedule,
-  setScheduleComplete
+  setScheduleComplete,
+  checkInSchedule,
+  checkOutSchedule
 } from "../../actions/scheduleActions";
 import isEmpty from "../../validation/is-empty";
 
@@ -67,119 +73,132 @@ class Schedule extends Component {
       let date = new Date(schedule.date);
       date = date.toLocaleString();
       scheduleContent = (
-        <div className="container">
-          <div className="card text-center border-dark">
-            <div className="card-header bg-dark text-white">Schedule</div>
-            <div className="card-body p-0 pt-2">
-              <div className="card-title text-primary">
-                <h2>
-                  <strong>{date}</strong>
-                </h2>
+        <div className="card text-center border-dark">
+          <CardHeader
+            links={[
+              <CardHeaderLink to="/schedule" text="Back to Schedule" />,
+              <div />
+            ]}
+            title={date}
+          />
+          <div className="card-body p-0">
+            <div className="list-group">
+              <div className="list-group-item">
+                <ListGroupItemh3p
+                  h3="Project"
+                  link={
+                    <Link to={`/projects/${schedule.project._id}`}>
+                      {schedule.project.projectname}
+                    </Link>
+                  }
+                />
               </div>
-              <div className="list-group">
-                <div className="list-group-item">
-                  <h3 className="card-text">
-                    Project:{" "}
-                    {(
-                      <Link to={`/projects/${schedule.project._id}`}>
-                        {schedule.project.projectname}
-                      </Link>
-                    ) || "No Project Provided"}
-                  </h3>
-                </div>
-
-                <div className="list-group-item">
-                  <h3 className="card-text">
-                    <u>Employees</u>
-                  </h3>
-                  <ul className="list-inline">
-                    {(schedule.employees &&
-                      schedule.employees.length > 0 &&
-                      schedule.employees.map(employee => (
-                        <li
-                          className="list-inline-item border border-secondary py-1 px-3 mb-2 rounded"
-                          key={employee._id}
-                        >
-                          <Link to={`/employees/${employee._id}`}>
-                            {employee.name}
-                          </Link>
-                        </li>
-                      ))) || (
-                      <li className="list-group-item">
-                        No Employees Scheduled
+              <div className="list-group-item">
+                <h3 className="card-text">
+                  <u>Employees</u>
+                </h3>
+                <ul className="list-inline">
+                  {(schedule.employees &&
+                    schedule.employees.length > 0 &&
+                    schedule.employees.map(employee => (
+                      <li
+                        className="list-inline-item border border-secondary py-1 px-3 mb-2 rounded"
+                        key={employee._id}
+                      >
+                        <Link to={`/employees/${employee._id}`}>
+                          {employee.name}
+                        </Link>
                       </li>
-                    )}
-                  </ul>
-                </div>
-                <div className="list-group-item">
-                  <h3>
-                    <u>Notes</u>
-                  </h3>
-                  <p>{schedule.notes || "No notes added."}</p>
-                </div>
-                <div className="list-group-item">
-                  <h3 className="card-text">
-                    Current Status:{" "}
-                    {schedule.isComplete ? (
-                      "Complete"
+                    ))) || (
+                    <li className="list-group-item">No Employees Scheduled</li>
+                  )}
+                </ul>
+              </div>
+              <div className="list-group-item">
+                <ListGroupItemh3p
+                  h3="Notes"
+                  pArray={[schedule.notes || "No notes added."]}
+                />
+              </div>
+              <div className="list-group-item">
+                <h3 className="card-text">
+                  <u>Status</u>
+                </h3>
+                {schedule.isComplete ? (
+                  "Complete"
+                ) : (
+                  <button
+                    onClick={this.completeSchedule.bind(this)}
+                    className="btn btn-outline-primary btn-lg"
+                    style={{ width: "15rem", transition: "all .5s" }}
+                    onMouseEnter={this.showTip.bind(this)}
+                    onMouseOut={this.hideTip.bind(this)}
+                  >
+                    {this.state.showTip ? "Click To Complete" : "Not Complete"}
+                  </button>
+                )}
+              </div>
+              <div className="list-group-item">
+                <div className="row mx-0">
+                  <div className="col-sm-6 mb-4 mb-sm-0 px-0">
+                    <h3>
+                      <u>Check-In</u>
+                    </h3>
+                    {schedule.checkin ? (
+                      <p>{new Date(schedule.checkin).toLocaleString()}</p>
                     ) : (
-                      <div style={{ display: "inline-block" }}>
-                        <button
-                          onClick={this.completeSchedule.bind(this)}
-                          className="btn btn-outline-primary btn-lg"
-                          style={{ width: "15rem", transition: "all .5s" }}
-                          onMouseEnter={this.showTip.bind(this)}
-                          onMouseOut={this.hideTip.bind(this)}
-                        >
-                          {this.state.showTip
-                            ? "Click To Complete"
-                            : "Not Complete"}
-                        </button>
-                      </div>
+                      <button
+                        className="btn btn-outline-primary btn-lg"
+                        onClick={() =>
+                          this.props.checkInSchedule(this.props.match.params.id)
+                        }
+                      >
+                        Check In
+                      </button>
                     )}
-                  </h3>
+                  </div>
+                  <div className="col-sm-6 px-0">
+                    <h3>
+                      <u>Check-Out</u>
+                    </h3>
+                    {schedule.checkout ? (
+                      <p>{new Date(schedule.checkout).toLocaleString()}</p>
+                    ) : (
+                      <button
+                        className="btn btn-outline-primary btn-lg"
+                        onClick={() =>
+                          this.props.checkOutSchedule(
+                            this.props.match.params.id
+                          )
+                        }
+                      >
+                        Check Out
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
+              {schedule.project.currentstatus !== "Completed" ? (
+                <CardFooter
+                  to={`/schedule/${this.props.match.params.id}/edit`}
+                  onClick={this.onShowModal.bind(this)}
+                />
+              ) : null}
             </div>
           </div>
-
-          <button
-            className="btn btn-secondary col-6 mt-2"
-            disabled={
-              schedule.project.currentstatus === "Completed" ? true : false
-            }
-            onClick={this.onEditSchedule.bind(this)}
-          >
-            Edit
-          </button>
-          <button
-            className="btn btn-dark col-6 mt-2"
-            onClick={this.onShowModal.bind(this)}
-          >
-            Remove
-          </button>
         </div>
       );
     }
 
     return (
-      <div className="container">
+      <div className="container mt-4 px-0 px-sm-3">
         <ConfirmRemoveModal
           show={this.state.showModal}
           onClose={this.onCloseModal.bind(this)}
           onConfirm={this.onDeleteSchedule.bind(this)}
         />
-        <div className="row">
-          <div className="col-md-2" />
-          <div className="ml-4">
-            <Link to="/schedule" className="btn btn-lg btn-primary ml-4 my-4">
-              Back To Schedule
-            </Link>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-8 m-auto">{scheduleContent}</div>
-        </div>
+
+        {scheduleContent}
       </div>
     );
   }
@@ -189,6 +208,8 @@ Schedule.propTypes = {
   getSchedule: PropTypes.func.isRequired,
   deleteSchedule: PropTypes.func.isRequired,
   setScheduleComplete: PropTypes.func.isRequired,
+  checkInSchedule: PropTypes.func.isRequired,
+  checkOutSchedule: PropTypes.func.isRequired,
   schedules: PropTypes.object.isRequired
 };
 
@@ -198,5 +219,11 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { getSchedule, deleteSchedule, setScheduleComplete }
+  {
+    getSchedule,
+    deleteSchedule,
+    setScheduleComplete,
+    checkInSchedule,
+    checkOutSchedule
+  }
 )(withRouter(Schedule));
