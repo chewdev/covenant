7bfa25456088c66309d1537ae597f82cf902518d
chewdev <itsbewald@gmail.com>
2018-11-17 23:51:30 -0800

    Update layout

--- client/src/components/common/TwoColumnItemRow.js
+++ client/src/components/common/TwoColumnItemRow.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function TwoColumnItemRow({ items }) {
   return (
-    <div className="row">
-      <div className="col-sm-6 mb-4 mb-sm-0">{items[0]}</div>
-      <div className="col-sm-6">{items[1]}</div>
+    <div className="row mx-0">
+      <div className="col-sm-6 mb-4 mb-sm-0 px-0">{items[0]}</div>
+      <div className="col-sm-6 px-0">{items[1]}</div>
     </div>
   );
 }
